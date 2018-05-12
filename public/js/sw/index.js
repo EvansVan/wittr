f768a37ced18d80d92d93f2b9536d6aa0cd6f6d3
Van Dabs <vandee762@icloud.com>
2018-05-12 12:16:27 +0300

    f

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,6 @@
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    new Response('Hello<strong class="a-winner-is-me"> World</strong>', {
+    new Response('Hello<b class="a-winner-is-me"> World</b>', {
       headers: {'Content-Type': 'text/html'}
     })
   );
