5ede7d8d251c4a17089ce034826b461f0d405a88
Van Dabs <vandee762@icloud.com>
2018-05-13 08:27:13 +0300

    caches

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -11,6 +11,16 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     // TODO: open a cache named 'wittr-static-v1'
     // Add cache the urls from urlsToCache
+    caches.open('wittr-staticv1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
   );
 });
 
